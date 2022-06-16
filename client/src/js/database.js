1ab4783a1e17ecf62d200c51c136363c3f0d5a35
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-06-16 18:28:47 -0500

    more database tweaks

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,9 +18,9 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({id: 1, content: content});
+  const request = store.put({id: 1, value: content});
   const result = await request;
-  console.log('result.value', result);
+  console.log('result.value', result.value);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -31,8 +31,8 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
-  console.log('result.content', result);
-  return result.content;
+  console.log('result.value', result.value);
+  return result.value;
 }
 
 initdb();
