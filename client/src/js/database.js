901a94c96c92f9e96103f5c3ea0d066e380064b5
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-06-18 09:48:54 -0500

    added null coalescer

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -31,8 +31,8 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.get(1);
   const result = await request;
-  console.log('result.value', result.value);
-  return result.value;
+  console.log('result.value', result?.value);
+  return result?.value;
 }
 
 initdb();
